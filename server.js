7d7da1503d6e068fdafd8b5d9acb0f13684c1875
Khalid Imran <khalid.imran.1404@gmail.com>
2021-01-15 20:01:16 +0600

    v0.5

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+/*
 //Install express server
+
 const express = require('express');
 const path = require('path');
 
@@ -7,10 +9,18 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
+app.get('/!*', function(req,res) {
 
   res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+*/
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
