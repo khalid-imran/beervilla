82ce0b478e5b3541fa9b282878ef9c93ea318964
Khalid Imran <khalid.imran.1404@gmail.com>
2021-01-15 20:42:24 +0600

    update read

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.listen(process.env.PORT || 8080);
 
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/!*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.use(express.static('./dist/<name-on-package.json>'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+  );
 });
+
 app.listen(process.env.PORT || 8080);
